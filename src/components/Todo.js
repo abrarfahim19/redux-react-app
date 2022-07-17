4d9bb2297d85e6303c5641a7542a3fd572fce753
abrarfahim19 <fahimpatwari19@gmail.com>
2022-07-18 00:15:23 +0600

    Data Fetch with Axios and Redux Thunk

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getAllTodos from '../services/actions/toDoAction';
 
 const Todo = () => {
+    const { isLoading, todos, error } = useSelector((state) => (state));
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllTodos())
+    }, [])
     return (
         <div>
             <h3>The Todo List</h3>
+            {isLoading && <h3>Loading...</h3>}
+            {error && <h3>{error.message}</h3>}
+            <section>
+                {todos && todos.map(todo => {
+                    return <h5 key={todo.id}>{todo.title}</h5>
+                })}
+            </section>
         </div>
     );
 };
--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import countReducer from "./services/reducers/countReducer";
+import todosReducer from "./services/reducers/todosReducer";
 
-const store = createStore(countReducer);
+// const store = createStore(countReducer);
+const store = createStore(todosReducer, applyMiddleware(thunk));
 
 export default store;
