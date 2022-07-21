727c348375f1644ed24669e94f7f54211cda4bdf
abrarfahim19 <fahimpatwari19@gmail.com>
2022-07-21 05:54:29 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 // import Counter from './components/Counter';
 // import Todo from './components/Todo';
-import { Route, Routes } from 'react-router-dom';
-import './App.css'
-import Home from './components/Home';
-import NavBar from './components/NavBar';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 import CounterView from "./features/counter/CounterView";
-import CrudView from './features/crud/CrudView';
-import EditCrud from './features/crud/EditCrud';
+import CrudView from "./features/crud/CrudView";
+import EditCrud from "./features/crud/EditCrud";
 import PostsView from "./features/posts/PostsView";
 
 function App() {
   return (
-    <div className='App'>
-      <NavBar/>
+    <div className="App">
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/counter' element={<CounterView/>}></Route>
-        <Route path='/posts' element={<PostsView/>}></Route>
-        <Route path='/crud' element={<CrudView/>}></Route>
-        <Route path='/edit-book' element={<EditCrud/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/counter" element={<CounterView />}></Route>
+        <Route path="/posts" element={<PostsView />}></Route>
+        <Route path="/crud" element={<CrudView />}></Route>
+        <Route path="/edit-book" element={<EditCrud />}></Route>
       </Routes>
     </div>
   );
--- src/features/crud/crudSlice.js
+++ src/features/crud/crudSlice.js
@@ -1,37 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const crudInit = {
-    books:[
-        {id:uuidv4(), name:"Self Help", author:"BugiChugi"},
-        {id:uuidv4(), name:"No Self Help", author:"ChugiBugi"},
-]
+  books: [
+    { id: uuidv4(), name: "Self Help", author: "BugiChugi" },
+    { id: uuidv4(), name: "No Self Help", author: "ChugiBugi" },
+  ],
 };
 
 const crudSlice = createSlice({
-    name: "crud",
-    initialState: crudInit,
-    reducers:{
-        showBooks: (state) => {return state},
-        addBooks: (state, action) => {
-            state.books.push(action.payload)
-        },
-        editBooks: (state, action) => {
-            const {id,name,author} = action.payload;
-            const isBookExist = state.books.filter((book)=>book.id === id)
-            if (isBookExist){
-                isBookExist[0].name = name;
-                isBookExist[0].author = author;
-            }
-        },
-        deleteBooks: (state, action) => {
-            const id = action.payload;
-            state.books = state.books.filter((book)=>book.id !== id)
-        },
+  name: "crud",
+  initialState: crudInit,
+  reducers: {
+    showBooks: (state) => {
+      return state;
+    },
+    addBooks: (state, action) => {
+      state.books.push(action.payload);
+    },
+    editBooks: (state, action) => {
+      const { id, name, author } = action.payload;
+      const isBookExist = state.books.filter((book) => book.id === id);
+      if (isBookExist) {
+        isBookExist[0].name = name;
+        isBookExist[0].author = author;
+      }
+    },
+    deleteBooks: (state, action) => {
+      const id = action.payload;
+      state.books = state.books.filter((book) => book.id !== id);
+    },
+  },
+});
 
-    }
-})
-
-export const {showBooks,addBooks,editBooks,deleteBooks} = crudSlice.actions;
+export const { showBooks, addBooks, editBooks, deleteBooks } =
+  crudSlice.actions;
 
 export default crudSlice.reducer;
